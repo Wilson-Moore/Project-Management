b76ebdd9760c5aca326e4d0d04d33889f7f8c8b8
Wilson-Moore <lordnothing02@gmail.com>
2025-04-20 18:37:25 +0100

    Added Auth functionality now for a basic UI

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WalletController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\SubProgramController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/signup',[AuthController::class,'signup']);
+Route::post('/login',[AuthController::class,'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/user',function (Request $request) {
+        return $request->user();
+    });
+});
 
-Route::group(['namespace'=>'App\Http\Controllers'],function(){
+Route::group(['namespace'=>'App\Http\Controllers','middleware'=>'auth:sanctum'],function(){
     Route::apiResource("wallets",WalletController::class);
     Route::apiResource("programs",ProgramController::class);
     Route::apiResource("subprograms",SubProgramController::class);
